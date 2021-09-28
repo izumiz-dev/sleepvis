1b86f75ea4168312012886ca67cfd7ecccc8a5d9
izumiz <42127166+izumiz-dev@users.noreply.github.com>
2021-09-29 02:09:02 +0900

    add ability to parse .csv file

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import raw from "raw.macro";
 
 function App() {
+  const rawCSV = raw("../AutoSleep-UTF8.csv"); //とりあえず自分のテストデータを入れる
+
+  interface ICol {
+    startTime: string; //  睡眠開始時刻
+    endTime: string; //  睡眠終了時刻
+    duration: string; //  Duration
+    sleepTime: string; //  睡眠時間
+    wakingInBed: string; //  ベッドで起きていた時間
+    timeToSleep: string; //  眠りにつくまでの時間
+    goodQuality: string; //  良質な睡眠
+    deepSleep: string; //  深い睡眠
+    heartRate: string; //  心拍数
+    tag: string; //  タグ
+    annotation: string; //  注釈
+  }
+
+  const rows: ICol[] = rawCSV
+    .split("\n")
+    .map((line) => {
+      const lineCols = line.split(",");
+      return {
+        startTime: lineCols[0],
+        endTime: lineCols[1],
+        duration: lineCols[2],
+        sleepTime: lineCols[3],
+        wakingInBed: lineCols[4],
+        timeToSleep: lineCols[5],
+        goodQuality: lineCols[6],
+        deepSleep: lineCols[7],
+        heartRate: lineCols[8],
+        tag: lineCols[9],
+        annotation: lineCols[10],
+      };
+    })
+    .slice(1);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {rows.map((col: ICol) => (
+        <div style={{ textAlign: "center" }}>
+          <span>Start: {col.startTime} </span>
+          <span>End: {col.endTime} </span>
+          <span>Duration: {col.duration} </span>
+        </div>
+      ))}
     </div>
   );
 }
