1777ad60304f7ee7187809dc500b09cbe5912c78
izumiz <42127166+izumiz-dev@users.noreply.github.com>
2021-10-06 21:38:28 +0900

    clean

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -12,94 +12,62 @@ export const Calendar = ({
   if (calendar) {
     return (
       <Days>
-        {calendar &&
-          calendar.flat().map((col: ICol | string) => {
-            if (typeof col !== "string" && col.endTime !== undefined) {
-              return (
-                <>
-                  <Frame>
-                    <DateHeader>
-                      <Date dayOfWeek={col.endTime.weekday}>
-                        {col.endTime.setLocale("jp").toFormat("MM.dd")}
-                      </Date>
-                      <TimesBox>
-                        <div>🛌{col.startTime.toFormat("HH:mm")}</div>
-                        <div>🛏{col.endTime.toFormat("HH:mm")}</div>
-                        <div>❤️{col.heartRate}</div>
-                      </TimesBox>
-                    </DateHeader>
-                    <div
-                      style={{
-                        display: "flex",
-                        flexDirection: "row",
-                        alignItems: "flex-end",
-                      }}
-                    >
-                      <Unit>
-                        <DayUnit
-                          duration={col.sleepTime}
-                          sleepType="sleepTime"
-                        />
-                      </Unit>
-                      <Unit>
-                        <DayUnit
-                          duration={col.goodQuality}
-                          sleepType="goodQuality"
-                        />
-                      </Unit>
-                      <Unit>
-                        <DayUnit
-                          duration={col.deepSleep}
-                          sleepType="deepSleep"
-                        />
-                      </Unit>
-                    </div>
-                  </Frame>
-                </>
-              );
-            } else {
-              return (
-                <Frame>
-                  <DateHeader>
-                    <Date
-                      dayOfWeek={
-                        DateTime.fromFormat(col.toString(), "yyyy-MM-dd-EEE")
-                          .weekday
-                      }
-                    >{`${col
-                      .toString()
-                      .substring(5, 10)
-                      .replace("-", ".")}`}</Date>
-                    <TimesBox>
-                      <div>🛌--:--</div>
-                      <div>🛏--:--</div>
-                      <div>❤️--</div>
-                    </TimesBox>
-                  </DateHeader>
-
-                  <div
-                    style={{
-                      display: "flex",
-                      flexDirection: "row",
-                    }}
-                  >
-                    <div
-                      style={{
-                        width: "204px",
-                        height: "100px",
-                        display: "flex",
-                        justifyContent: "center",
-                        alignContent: "center",
-                        color: "rgba(0,0,0,0.5)",
-                      }}
-                    >
-                      データがありません
-                    </div>
-                  </div>
-                </Frame>
-              );
-            }
-          })}
+        {calendar.flat().map((col: ICol | string) => {
+          if (typeof col !== "string" && col.endTime !== undefined) {
+            return (
+              <Frame>
+                <DateHeader>
+                  <Date dayOfWeek={col.endTime.weekday}>
+                    {col.endTime.setLocale("jp").toFormat("MM.dd")}
+                  </Date>
+                  <TimesBox>
+                    <div>🛌{col.startTime.toFormat("HH:mm")}</div>
+                    <div>🛏{col.endTime.toFormat("HH:mm")}</div>
+                    <div>❤️{col.heartRate}</div>
+                  </TimesBox>
+                </DateHeader>
+                <GraphsBox>
+                  <Unit>
+                    <DayUnit duration={col.sleepTime} sleepType="sleepTime" />
+                  </Unit>
+                  <Unit>
+                    <DayUnit
+                      duration={col.goodQuality}
+                      sleepType="goodQuality"
+                    />
+                  </Unit>
+                  <Unit>
+                    <DayUnit duration={col.deepSleep} sleepType="deepSleep" />
+                  </Unit>
+                </GraphsBox>
+              </Frame>
+            );
+          } else {
+            return (
+              <Frame>
+                <DateHeader>
+                  <Date
+                    dayOfWeek={
+                      DateTime.fromFormat(col.toString(), "yyyy-MM-dd-EEE")
+                        .weekday
+                    }
+                  >{`${col
+                    .toString()
+                    .substring(5, 10)
+                    .replace("-", ".")}`}</Date>
+                  <TimesBox>
+                    <div>🛌--:--</div>
+                    <div>🛏--:--</div>
+                    <div>❤️--</div>
+                  </TimesBox>
+                </DateHeader>
+                <NoDataOuter>
+                  <NoDataComponent>データがありません</NoDataComponent>
+                </NoDataOuter>
+              </Frame>
+            );
+          }
+        })}
       </Days>
     );
   }
@@ -123,4 +91,24 @@ const TimesBox = styled.div`
   font-size: 0.75rem;
   font-weight: 500;
   margin-left: 1rem;
+`;
+
+const GraphsBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-end;
+`;
+
+const NoDataComponent = styled.div`
+  width: 204px;
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  color: rgba(0, 0, 0, 0.5);
+`;
+
+const NoDataOuter = styled.div`
+  display: flex;
+  flex-direction: row;
 `;
